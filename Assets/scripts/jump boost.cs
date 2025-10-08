6a29208e97b09bb74ad57d2b8513cd96dcd74693
fin-netizen <564242@escg.ac.uk>
2025-10-08 13:42:12 +0100

    adding stuff

--- Assets/scripts/jump boost.cs
+++ Assets/scripts/jump boost.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class jumpboost : MonoBehaviour
 {
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float yvel; 
+        yvel = rb.linearVelocity.y;
     }
 }
